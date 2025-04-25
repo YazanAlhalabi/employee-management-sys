b708fa2bf277478f0bb0b6b31ded55a147b362a9
YazanAlhalabi <yazanalhalabi1@gmail.com>
2025-04-26 01:50:32 +0300

    step 16 - add CORS

--- src/main/java/com/myFullstackYazan/employee_management/config/SecurityConfig.java
+++ src/main/java/com/myFullstackYazan/employee_management/config/SecurityConfig.java
@@ -15,6 +15,11 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
 
 @Configuration
 @EnableWebSecurity
@@ -33,14 +38,18 @@ public class SecurityConfig {
   @Bean
   public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
     http
-        .cors(AbstractHttpConfigurer::disable)
+        .cors(c -> {
+          CorsConfigurationSource source = corsConfigurationSource();
+          c.configurationSource(source);
+        })
         .csrf(AbstractHttpConfigurer::disable)
         .authorizeHttpRequests(auth -> {
           auth.requestMatchers(
                   "/auth/singup",
-                  "auth/login"
+                  "auth/login",
+                  "employees"
               ).permitAll()
-              .requestMatchers(HttpMethod.GET, "/employees").hasRole("ADMIN")
+//              .requestMatchers(HttpMethod.GET, "/employees").hasRole("ADMIN")
               .requestMatchers(HttpMethod.GET, "/employees/{employeeId}").hasAnyRole("ADMIN", "USER")
               .requestMatchers(HttpMethod.POST, "/employees").hasAnyRole("ADMIN")
               .requestMatchers(HttpMethod.DELETE, "/employees/{employeeId}").hasAnyRole("ADMIN")
@@ -63,5 +72,18 @@ public class SecurityConfig {
     authBuilder.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
 
     return authBuilder.build();
+  }
+
+  @Bean
+  public CorsConfigurationSource corsConfigurationSource() {
+    CorsConfiguration configuration = new CorsConfiguration();
+    configuration.setAllowedOrigins(List.of("http://localhost:5173"));
+    configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
+    configuration.setAllowedHeaders(List.of("*"));
+    configuration.setAllowCredentials(true);
+
+    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+    source.registerCorsConfiguration("/**", configuration);
+    return source;
   }
 }
